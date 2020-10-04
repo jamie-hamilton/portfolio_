2d54901f109952d5a0a17d2cef49c08bebae7f10
Jamie Hamilton <jhamilton.contact@gmail.com>
2020-10-04 19:42:27 +0100

    :gear: more webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -13,5 +13,24 @@ module.exports = {
         publicPath: "./frontend/static/frontend",
         filename: "main.js",
         chunkFilename: "[id]-[chunkhash].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                loader: "babel-loader",
+                options: { 
+                    presets: ['@babel/preset-env', '@babel/react'],
+                    plugins:['@babel/plugin-proposal-class-properties']
+                }
+                }
+            }
+        ]
     }
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,11 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const BrotliPlugin = require('brotli-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
    mode: 'production',
    devtool: '',
    plugins: [
-      new MiniCssExtractPlugin({
-        filename: '[name].css',
-        chunkFilename: '[id].css'
-      }),
 		new BrotliPlugin({
 			asset: '[path].br[query]',
 			test: /\.(js|css|html|svg)$/,
@@ -18,23 +13,4 @@ module.exports = merge(common, {
 			minRatio: 0.8
 		})
    ],
-   module: {
-      rules: [
-          {
-              test: /\.scss$/,
-              use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-          },
-          {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-              loader: "babel-loader",
-              options: { 
-                  presets: ['@babel/preset-env', '@babel/react'],
-                  plugins:['@babel/plugin-proposal-class-properties']
-              }
-              }
-          }
-      ]
-  }
 });
