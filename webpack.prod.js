84b28ca671d3c5806ac9e016e6b9fd9f0f180f97
Jamie Hamilton <jhamilton.contact@gmail.com>
2020-10-04 20:37:54 +0100

    :gear: more webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const BrotliPlugin = require('brotli-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
    mode: 'production',
