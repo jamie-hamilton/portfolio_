de9d6b06cbdb1e7c62a5ed9d9588fc59f30a5ef1
Jamie Hamilton <jhamilton.contact@gmail.com>
2020-10-05 09:05:15 +0100

    :gear: added webpack.DefinePlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const webpack = require('webpack');
 const common = require('./webpack.common.js');
 const BrotliPlugin = require('brotli-webpack-plugin');
 
