35f3f715885e6dc44fef632a86e00dd994177245
Jamie Hamilton <jhamilton.contact@gmail.com>
2020-10-05 08:55:37 +0100

    :gear: added webpack.DefinePlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,14 @@ module.exports = merge(common, {
    mode: 'production',
    devtool: '',
    plugins: [
+		new webpack.DefinePlugin({
+			PRODUCTION: JSON.stringify(true),
+			VERSION: JSON.stringify('5fa3b9'),
+			BROWSER_SUPPORTS_HTML5: true,
+			TWO: '1+1',
+			'typeof window': JSON.stringify('object'),
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+		}),
 		new BrotliPlugin({
 			asset: '[path].br[query]',
 			test: /\.(js|css|html|svg)$/,
